1a481b1f192771285fb4caca4cb232425733867f
Mike Johnson <m1chael.johnson1804@gmail.com>
2021-02-03 15:01:43 +0000

    Create dionaea-deployment-linode.sh
    This script will deploy dionaea and connect it to MHN on a linode system

---
+++ dionaea-deployment-linode.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ $# -ne 2 ]
+    then
+        echo "Wrong number of arguments supplied."
+        echo "Usage: $0 <server_url> <deploy_key>."
+        exit 1
+fi
+
+server_url=$1
+deploy_key=$2
+
+# Install dependencies
+apt update
+apt --yes install \
+    git \
+    supervisor \
+    build-essential \
+    cmake \
+    check \
+    cython3 \
+    libcurl4-openssl-dev \
+    libemu-dev \
+    libev-dev \
+    libglib2.0-dev \
+    libloudmouth1-dev \
+    libnetfilter-queue-dev \
+    libnl-3-dev \
+    libpcap-dev \
+    libssl-dev \
+    libtool \
+    libudns-dev \
+    python3 \
+    python3-dev \
+    python3-bson \
+    python3-yaml \
+    python3-boto3 
+
+git clone https://github.com/DinoTools/dionaea.git 
+cd dionaea
+
+# Latest tested version with this install script
+git checkout baf25d6
+
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/opt/dionaea ..
+
+make
+make install
+
+wget $server_url/static/registration.txt -O registration.sh
+chmod 755 registration.sh
+# Note: this will export the HPF_* variables
+. ./registration.sh $server_url $deploy_key "dionaea"
+
+cat > /opt/dionaea/etc/dionaea/ihandlers-enabled/hpfeeds.yaml <<EOF
+- name: hpfeeds
+  config:
+    # fqdn/ip and port of the hpfeeds broker
+    server: "YOUR_SERVER_IP"
+    # port: $HPF_PORT
+    ident: "$HPF_IDENT"
+    secret: "$HPF_SECRET"
+    # dynip_resolve: enable to lookup the sensor ip through a webservice
+    dynip_resolve: "http://canhazip.com/"
+    # Try to reconnect after N seconds if disconnected from hpfeeds broker
+    # reconnect_timeout: 10.0
+EOF
+
+cat > /opt/dionaea/etc/dionaea/ihandlers-enabled/virustotal.yaml <<EOF
+- name: virustotal
+  config:
+    # grab it from your virustotal account at My account -> My API Key (https://www.virustotal.com/en/user/<username>/apikey/)
+    apikey: "YOUR_VIRUSTOTAL_API"
+    file: "var/lib/dionaea/vtcache.sqlite"
+    comment: "This sample was captured in the wild and uploaded by the dionaea honeypot.\n#honeypot #malware #networkworm"
+EOF
+
+
+# Disable ipv6
+cat > /etc/sysctl.conf <<EOF
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+
+echo 'AddressFamily inet' | sudo tee -a /etc/ssh/sshd_config
+
+cat > /opt/dionaea/etc/dionaea/dionaea.cfg <<EOF
+[dionaea]
+download.dir=var/lib/dionaea/binaries/
+modules=curl,python,nfq,emu,pcap
+processors=filter_streamdumper,filter_emu
+
+listen.mode=getifaddrs
+# listen.addresses=127.0.0.1
+# listen.interfaces=eth0,tap0
+
+# Use IPv4 mapped IPv6 addresses
+# It is not recommended to use this feature, try to use nativ IPv4 and IPv6 adresses
+# Valid values: true|false
+# listen.use_ipv4_mapped_ipv6=false
+
+# Country
+# ssl.default.c=GB
+# Common Name/domain name
+# ssl.default.cn=
+# Organization
+# ssl.default.o=
+# Organizational Unit
+# ssl.default.ou=
+
+[logging]
+default.filename=var/log/dionaea/dionaea.log
+default.levels=error
+default.domains=*
+
+errors.filename=var/log/dionaea/dionaea-errors.log
+errors.levels=error
+errors.domains=*
+
+[processor.filter_emu]
+name=filter
+config.allow.0.protocols=smbd,epmapper,nfqmirrord,mssqld
+next=emu
+
+[processor.filter_streamdumper]
+name=filter
+config.allow.0.types=accept
+config.allow.1.types=connect
+config.allow.1.protocols=ftpctrl
+config.deny.0.protocols=ftpdata,ftpdatacon,xmppclient
+next=streamdumper
+
+[processor.streamdumper]
+name=streamdumper
+config.path=var/lib/dionaea/bistreams/%Y-%m-%d/
+
+[processor.emu]
+name=emu
+config.limits.files=3
+#512 * 1024
+config.limits.filesize=524288
+config.limits.sockets=3
+config.limits.sustain=120
+config.limits.idle=30
+config.limits.listen=30
+config.limits.cpu=120
+#// 1024 * 1024 * 1024
+config.limits.steps=1073741824
+
+[module.nfq]
+queue=2
+
+[module.nl]
+# set to yes in case you are interested in the mac address  of the remote (only works for lan)
+lookup_ethernet_addr=no
+
+[module.python]
+imports=dionaea.log,dionaea.services,dionaea.ihandlers
+sys_paths=default
+service_configs=etc/dionaea/services-enabled/*.yaml
+ihandler_configs=etc/dionaea/ihandlers-enabled/*.yaml
+
+[module.pcap]
+any.interface=any
+EOF
+
+cat > /opt/bistream_script.sh <<EOF
+#!/bin/bash
+
+# Compress bistream files older than 3 Hours
+find /opt/dionaea/var/lib/dionaea/bistreams/* -type f -mmin +360 -exec gzip {} \;
+
+# Clear bistream logs from dionaea every 6 Hours
+find /opt/dionaea/var/lib/dionaea/bistreams/* -type f -mmin +720 -exec rm -rf {} \;
+EOF
+
+crontab -l | { cat; echo "0 * * * * /bin/bash /opt/bistream_script.sh"; } | crontab -
+
+# Editing configuration for Dionaea.
+mkdir -p /opt/dionaea/var/log/dionaea/wwwroot /opt/dionaea/var/log/dionaea/binaries /opt/dionaea/var/log/dionaea/log
+chown -R nobody:nogroup /opt/dionaea/var/log/dionaea
+
+mkdir -p /opt/dionaea/var/log/dionaea/bistreams 
+chown nobody:nogroup /opt/dionaea/var/log/dionaea/bistreams
+
+# Config for supervisor.
+cat > /etc/supervisor/conf.d/dionaea.conf <<EOF
+[program:dionaea]
+command=/opt/dionaea/bin/dionaea -c /opt/dionaea/etc/dionaea/dionaea.cfg
+directory=/opt/dionaea/
+stdout_logfile=/opt/dionaea/var/log/dionaea.out
+stderr_logfile=/opt/dionaea/var/log/dionaea.err
+autostart=true
+autorestart=true
+redirect_stderr=true
+stopsignal=QUIT
+EOF
+
+supervisorctl update
+
+reboot now
